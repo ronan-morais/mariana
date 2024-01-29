669744c35eda01865e70c462fb50378da706d8f5
Ronan <ronan86@gmail.com>
2024-01-29 21:06:29 +1030

    responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				jost: ['"Jost"'],
+			},
+			colors: {
+				// Using modern `rgb`
+				marromTronco: "rgba(170, 145, 117, 1)",
+				marromTitulo: "rgba(89, 84, 82, 1)",
+				begeCopa: "rgba(226, 221, 200, 1)",
+			},
+		},
 	},
 	plugins: [],
 }
