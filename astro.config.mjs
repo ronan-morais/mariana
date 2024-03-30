a918c40c2a03c18cb40a7465229dc4145b1a7e62
Ronan <ronan86@gmail.com>
2024-03-30 22:23:04 +1030

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
-
-//import tailwind from "@astrojs/tailwind";
-
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
-
 import db from "@astrojs/db";
+//import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://marianasilveira.vercel.app/",
   integrations: [tailwind(), solidJs(), db()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel()
 } //integrations: [tailwind()]
 );
