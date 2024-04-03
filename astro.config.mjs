fa167c230a673c76c1c4bf67a0ac7281b05c7fc3
Ronan <ronan86@gmail.com>
2024-04-03 15:51:27 +1030

    dev auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
 import db from "@astrojs/db";
 
+import auth from "auth-astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://marianasilveira.vercel.app/",
-  integrations: [tailwind(), solidJs(), db()],
+  integrations: [tailwind(), solidJs(), db(), auth()],
   output: "server",
   adapter: vercel({
-    imageService: true,
-  }),
+    imageService: true
+  })
 });
