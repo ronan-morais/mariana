0974f2dff1198e78e8c1c3503efd4231c30f65ce
Ronan <ronan86@gmail.com>
2024-04-04 14:34:37 +1030

    auth login credentials

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,11 +1,23 @@
 import GitHub from "@auth/core/providers/github";
+import Credentials from "@auth/core/providers/credentials"
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
   providers: [
-    GitHub({
+    /* GitHub({
       clientId: import.meta.env.GITHUB_CLIENT_ID,
       clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    }) */
+    Credentials({
+      credentials: {
+        username: { label: "username" },
+        password: { label: "password", type: "password" }
+      },
+      async authorize() {
+        return await ({ logged: true })
+      }
     })
-  ]
+  ],
+  secret: "akldjfoiaudfaksjdfiuoew3oui12iouweq908suijkl213oijklwpqsa",
+  trustHost: true,
 })
