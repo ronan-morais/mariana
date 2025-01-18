dc373b4cabfcb50856459685982648fbc002584d
Ronan Alves de Morais <ronan86@gmail.com>
2025-01-18 16:40:26 +1030

    db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
-import db from "@astrojs/db";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://marianasilveira.vercel.app/",
-  integrations: [tailwind(), db(), react()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel({
     imageService: true,
