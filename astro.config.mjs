23e6a9a01f257edd26185c48903f3ec4a7dfb4b8
Ronan Alves de Morais <ronan86@gmail.com>
2025-02-02 01:58:44 +1030

    vercel 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.marianacastropsicologa.com.br/",
+  site: "https://marianasilveira.vercel.app/",
   integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel,
