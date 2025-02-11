3f0489027e95a3c9223ccd58b83c63c8317d205d
Ronan Alves de Morais <ronan86@gmail.com>
2025-02-11 16:26:28 +1030

    node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.marianacastropsicologa.com.br/",
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
